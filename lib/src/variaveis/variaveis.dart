d2e32a8385a1ccae4d8982006cb373a592b4088c
Junior Abranches <juniorabranches@gmail.com>
2018-10-02 00:11:33 -0400

    Modulo 2 - Aula 4

--- lib/src/variaveis/variaveis.dart
+++ lib/src/variaveis/variaveis.dart
@@ -13,6 +13,15 @@ import 'package:angular_components/angular_components.dart';
 
 class VariaveisComponent{
   clickFab(){
-    
+    String nome = "Curso de Dart";
+    int frutas = 5;
+    double preco = 10.5;
+    bool isCurso = true;
+
+    frutas = 10;
+
+    print("O nome do meu curso é: $nome");
+    print("A quantidade de frutas que eu comprei foi: $frutas");
+    print("O preço total das frutas compradas, foi de $preco");
   }
 }
