5aa7ebdc4659f828f644877a87332017d56f401e
Junior Abranches <juniorabranches@gmail.com>
2018-10-05 23:39:09 -0400

    Modulo 6 - Aula 4

--- lib/src/tarefas/tarefas.dart
+++ lib/src/tarefas/tarefas.dart
@@ -1,8 +1,9 @@
 import 'package:angular/angular.dart';
 
-import 'package:angular/angular.dart';
+import 'dart:async';
 import 'package:angular_components/angular_components.dart';
 
+import 'tarefas_service.dart';
 @Component(
   selector: 'tarefas',
   templateUrl: 'tarefas.html',
@@ -13,9 +14,24 @@ import 'package:angular_components/angular_components.dart';
     MaterialButtonComponent,
     NgFor, NgIf,
     materialInputDirectives
-  ]
+  ],
+  providers: [const ClassProvider(TarefasService)]
 )
 
-class TarefasComponent{
- 
+class TarefasComponent implements OnInit{
+  final TarefasService tarefasService;
+
+  List<String> items = [];
+  String novaTarefa = '';
+
+  TarefasComponent(this.tarefasService);
+  @override 
+  Future<Null> ngOnInit() async {
+    items = await tarefasService.getTarefas();
+  }
+
+  void add(){
+    items.add(novaTarefa);
+    novaTarefa = '';
+  }
 }
